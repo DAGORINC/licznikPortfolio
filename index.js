0440ef2b38d238f12c447af8bb647547328b60dd
dagor100 <damianxe@onet.pl>
2023-10-12 18:42:48 +0200

    add https

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const https = require('https');
+const fs = require('fs');
 
 const app = express();
 const port = 3000;
